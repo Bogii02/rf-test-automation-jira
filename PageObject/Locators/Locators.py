8e3f44d430e17d5218a6dd9f6c773a791e7e874b
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-05-21 14:24:24 +0200

    get locators from pages

---
+++ PageObject/Locators/Locators.py
@@ -0,0 +1,9 @@
+# Login Page locators
+LoginUsernameInputBox = "id:login-form-username"
+LoginPasswordInputBox = "id:login-form-password"
+LoginButton = "id:login-form-submit"
+
+# Home Page locators
+IntroductionPanel = "id:rep-10000"
+UserMenu = "id:header-details-user-fullname"
+LogoutButton = "id:system"
