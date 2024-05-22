f34d2d2fbbca07c232ac90e27bc74c8bbe65a0b1
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-05-22 13:16:53 +0200

    test

--- PageObject/TestData/TestData.py
+++ PageObject/TestData/TestData.py
@@ -7,3 +7,7 @@ JIRA_USERNAME = os.getenv("JIRA_USERNAME")
 JIRA_FULL_NAME = os.getenv("JIRA_FULL_NAME")
 JIRA_PASSWORD = os.getenv("JIRA_PASSWORD")
 SITE_URL = os.getenv("SITE_URL")
+
+print(JIRA_PASSWORD)
+print(JIRA_USERNAME)
+print(SITE_URL)
