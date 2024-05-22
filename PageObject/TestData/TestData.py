e96127dbbc3e33f81c91ae6cf281c64f6afcdce0
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-05-22 13:30:05 +0200

    test

--- PageObject/TestData/TestData.py
+++ PageObject/TestData/TestData.py
@@ -3,11 +3,7 @@ import os
 
 load_dotenv()
 
+SITE_URL = os.getenv("SITE_URL")
 JIRA_USERNAME = os.getenv("JIRA_USERNAME")
 JIRA_FULL_NAME = os.getenv("JIRA_FULL_NAME")
 JIRA_PASSWORD = os.getenv("JIRA_PASSWORD")
-SITE_URL = os.getenv("SITE_URL")
-
-print(JIRA_PASSWORD)
-print(JIRA_USERNAME)
-print(SITE_URL)
