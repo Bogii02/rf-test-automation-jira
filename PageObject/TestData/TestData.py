efe6fd3493b3fb7c4f7810ef6a51fc1ebc1d939a
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-05-22 09:46:19 +0200

    create TestData.py with environment variables

---
+++ PageObject/TestData/TestData.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+JIRA_USERNAME = os.getenv("JIRA_USERNAME")
+JIRA_FULL_NAME = os.getenv("JIRA_FULL_NAME")
+JIRA_PASSWORD = os.getenv("JIRA_PASSWORD")
+SITE_URL = os.getenv("SITE_URL")
