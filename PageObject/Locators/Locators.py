cccb6ddedef191fdf524c5fc2840b98e31d1866d
Boglarka Bakos <53225139+Bogii02@users.noreply.github.com>
2024-05-23 13:31:59 +0200

    add locators for creating and deleting an issue

--- PageObject/Locators/Locators.py
+++ PageObject/Locators/Locators.py
@@ -9,3 +9,15 @@ IntroductionPanel = "id:rep-10000"
 UserMenu = "id:header-details-user-fullname"
 LoginAgainButton = "xpath://*[@id='main']/div/div/p[2]/a"
 LogoutButton = "id:system"
+CreateButton = "id:create-menu"
+
+# Create issue locators
+SummaryField = "id:summary"
+CreateIssueSubmitButton = "id:create-issue-submit"
+IssueName = "xpath://*[@id=\"aui-flag-container\"]/div/div/a"
+
+# Issue Details locators
+DropdownMenu = "xpath://*[@id=\"opsbar-operations_more\"]/span"
+DeleteButton = "id:delete-issue"
+SubmitIssueDeletionBtn = "id:delete-issue-submit"
+SummaryValue = "id:summary-val"
